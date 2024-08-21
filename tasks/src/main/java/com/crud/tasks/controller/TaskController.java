d444af309f3bcc5ab4f9342ae7803e3561edde05
unknown <twardym78@gmail.com>
2024-08-21 12:25:54 +0200

    15.2 - wyszukiwanie pojedynczego zadania po id

--- tasks/src/main/java/com/crud/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,21 +1,33 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
-    @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "New Title", "New Content");
+
+    @GetMapping(value = "{id}")
+    public TaskDto getTask(@PathVariable int id) {
+        Task task = service.getTaskById(id);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @DeleteMapping
---
+++ tasks/src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class DbService {
+
+    private final TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task getTaskById(int id) {
+        return repository.findById(id);
+    }
+
+}
