a4c9523c4cd249b5b584d02f70852546c1f17813
unknown <twardym78@gmail.com>
2024-10-12 14:41:11 +0200

    21.4 - rozbudowa endpointa.

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -17,7 +17,7 @@ public class EmailScheduler {
     private final AdminConfig adminConfig;
     private static final String SUBJECT = "Tasks: Once a day email";
 
-    @Scheduled(fixedDelay = 10000)//cron = "0 0 10 * * *"
+    @Scheduled(cron = "0 0 8 * * *")//cron = "0 0 10 * * *"
     public void sendInformationEmail() {
         long size = taskRepository.count();
         if (size == 1) {
